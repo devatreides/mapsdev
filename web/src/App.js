26308dce07ea107a2d84ecb90755b242bd872450
Elton Benevides <eltonfelixbenevides@gmail.com>
2020-01-17 01:41:30 -0400

    frontend web interface design built

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,111 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import './global.css';
 import './App.css';
+import './Sidebar.css';
+import './Main.css';
+
 
 function App() {
+
+  const [github_username, setGithubUsername] = useState('');
+  const [techs, setTechs] = useState('');
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords
+
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (err) => {
+        console.log(err)
+      },
+      {
+        timeout: 30000
+      }
+    )
+  }, [])
+
+  async function handleAddDev(e) {
+    e.preventDefault();
+
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form onSubmit={handleAddDev}>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do GitHub</label>
+            <input 
+              name="github_username" 
+              id="github_username"
+              value={github_username}
+              onChange={e => setGithubUsername(e.target.value)} 
+              required 
+            />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input 
+              name="techs" 
+              id="techs"
+              value={techs} 
+              onChange={e => setTechs(e.target.value)} 
+              required 
+            />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input 
+                type="number" 
+                name="latitude" 
+                id="latitude" 
+                value={latitude} 
+                onChange={e => setLatitude(e.target.value)}
+                required
+              />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input 
+                type="number" 
+                name="longitude" 
+                id="longitude" 
+                value={longitude} 
+                onChange={e => setLongitude(e.target.value)}
+                required 
+              />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/18440704?s=460&v=4" alt="Tom Benevides"/>
+              <div className="user-info">
+                <strong>Tom Benevides</strong>
+                <span>React Native, Dart</span>
+              </div>
+            </header>
+            <p>Bachelor in Computer Science and development technician. Work in IT departament at UNEMAT, Brazil</p>
+            <a href="https://github.com/tomfbenevides">Acessar perfil no GitHub</a>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
