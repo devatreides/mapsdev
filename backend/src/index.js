f216f4550ffa98a35819ac71c8d92c13c619b65c
Elton Benevides <eltonfelixbenevides@gmail.com>
2020-01-17 01:41:58 -0400

    frontend web interface design built

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const mongooseKey = require('../credentials/mongoose.json');
 const routes = require('./routes');
 
@@ -10,6 +11,7 @@ mongoose.connect(mongooseKey.connection, {
     useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
