5a16beafe92ee59e76a806eea65e6359dbc9658a
Elton Benevides <eltonfelixbenevides@gmail.com>
2020-01-18 22:23:27 -0400

    Frontend web finished. Working with axios api and components

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import api from './services/api';
+
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
+
 import './global.css';
 import './App.css';
 import './Sidebar.css';
@@ -7,31 +12,23 @@ import './Main.css';
 
 function App() {
 
-  const [github_username, setGithubUsername] = useState('');
-  const [techs, setTechs] = useState('');
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
+  const [devs, setDevs] = useState([])
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords
+    async function loadDevs() {
+      const response = await api.get('/devs');
 
-        setLatitude(latitude);
-        setLongitude(longitude);
-      },
-      (err) => {
-        console.log(err)
-      },
-      {
-        timeout: 30000
-      }
-    )
+      setDevs(response.data)
+    }
+
+    loadDevs()
   }, [])
 
-  async function handleAddDev(e) {
-    e.preventDefault();
+  async function handleAddDev(data) {
 
+    const response = await api.post('/devs', data);
+
+    setDevs([...devs, response.data])
     
   }
 
@@ -39,71 +36,13 @@ function App() {
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form onSubmit={handleAddDev}>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do GitHub</label>
-            <input 
-              name="github_username" 
-              id="github_username"
-              value={github_username}
-              onChange={e => setGithubUsername(e.target.value)} 
-              required 
-            />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input 
-              name="techs" 
-              id="techs"
-              value={techs} 
-              onChange={e => setTechs(e.target.value)} 
-              required 
-            />
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input 
-                type="number" 
-                name="latitude" 
-                id="latitude" 
-                value={latitude} 
-                onChange={e => setLatitude(e.target.value)}
-                required
-              />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input 
-                type="number" 
-                name="longitude" 
-                id="longitude" 
-                value={longitude} 
-                onChange={e => setLongitude(e.target.value)}
-                required 
-              />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm  onSubmit={handleAddDev} />
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/18440704?s=460&v=4" alt="Tom Benevides"/>
-              <div className="user-info">
-                <strong>Tom Benevides</strong>
-                <span>React Native, Dart</span>
-              </div>
-            </header>
-            <p>Bachelor in Computer Science and development technician. Work in IT departament at UNEMAT, Brazil</p>
-            <a href="https://github.com/tomfbenevides">Acessar perfil no GitHub</a>
-          </li>
+          {devs.map(dev => (
+            <DevItem  key={dev._id} dev={dev} />
+          ))}
         </ul>
       </main>
     </div>
